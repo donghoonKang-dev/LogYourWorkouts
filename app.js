9966267005e8ba83d4280545b260ac87f655d2ac
donghoonKang-dev <donghoon.dev@gmail.com>
2021-01-07 19:32:33 +0900

    communication designed

--- app.js
+++ app.js
@@ -2,23 +2,55 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const port = 8080;
 
+// DB setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.MONGO_DB);
+let db = mongoose.connection;
+//db connect success
+db.once('open', function(){
+  console.log('DB connected');
+});
 app.use(cors());
+//db connect fail
+db.on('error', function(err){
+  console.log('DB ERROR : ', err);
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//DB schema
+let logSchema = mongoose.Schema({
+  name:{type:String, required:true, unique:true},
+  date:{type:Date, required:true},
+  workout:{type:String, required:true}
+});
+let workoutLog = mongoose.model('workoutLog', logSchema);
+let data = 'no data';
 
 app.get('/', (req, res) => {
   res.render('index');
+})
+
+app.get('/stack', (req, res) => {
+  res.send(data)
+})
+
+app.post('/stack', (req, res) => {
+  data = req.body
+  console.log(`data: ${data}`)
+  res.send('ok')
 })
 
 app.listen(port, () => {
