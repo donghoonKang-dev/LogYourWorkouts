15c41e533b89b166c0cec80b1562db80eeffcb83
donghoonKang-dev <donghoon.dev@gmail.com>
2021-01-11 21:28:47 +0900

    DB Connecting - CREATE

--- app.js
+++ app.js
@@ -12,17 +12,17 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect(process.env.MONGO_DB);
-let db = mongoose.connection;
+const db = mongoose.connection;
 //db connect success
 db.once('open', function(){
   console.log('DB connected');
 });
-app.use(cors());
 //db connect fail
 db.on('error', function(err){
   console.log('DB ERROR : ', err);
 });
 
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //DB schema
 let logSchema = mongoose.Schema({
-  name:{type:String, required:true, unique:true},
+  name:{type:String, required:true},
   date:{type:Date, required:true},
   workout:{type:String, required:true}
 });
@@ -43,14 +43,36 @@ app.get('/', (req, res) => {
   res.render('index');
 })
 
-app.get('/stack', (req, res) => {
-  res.send(data)
+app.get('/workout', (req, res) => {
+  res.send(data);
 })
 
-app.post('/stack', (req, res) => {
-  data = req.body
-  console.log(`data: ${data}`)
-  res.send('ok')
+app.get('/workout/:name', (req, res) => {
+  workoutLog.find({name : req.params.name}, {name:1, date:1, workout:1}, function(err, workouts){
+    if(err) return res.status(500).json({error:error});
+    if(workouts.length === 0) return res.status(404).json({error: 'workoutlog not found'});
+    res.json(workouts);
+  });
+})
+
+app.post('/workout', (req, res) => {
+  data = req.body;
+  /*
+  console.log(`data: ${data}`);
+  res.send('okay');
+  */
+ let workoutData = new workoutLog();
+ workoutData.name = req.body.name;
+ workoutData.date = req.body.date;
+ workoutData.workout = req.body.workout;
+workoutData.save(function(err){
+  if(err){
+    console.error(err);
+    res.json({result:0});
+    return;
+  }
+  res.json({result:1});
+  });
 })
 
 app.listen(port, () => {
