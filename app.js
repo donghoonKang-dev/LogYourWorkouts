d57d37cc8459110c1be3fa3ad4b4ee26e17e690a
donghoonKang-dev <donghoon.dev@gmail.com>
2021-03-07 19:24:29 +0900

    Cleaned up codes

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const port = 8080;
+const port = process.env.port||8080;
 
 app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static('public'));
 
-// DB setting
+//<==========DB Setting==========>
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
@@ -22,7 +21,7 @@ mongoose.connect(process.env.MONGO_DB);
 const db = mongoose.connection;
 //DB connect success
 db.once('open', function(){
-  console.log('DB connected');
+  console.log('MongoDB successfully connected');
 });
 //DB connect fail
 db.on('error', function(err){
@@ -36,18 +35,18 @@ let logSchema = mongoose.Schema({
 });
 let workoutLog = mongoose.model('workoutLog', logSchema);
 
-/////Routing
+//<==========Routing==========>
 app.get('/', (req, res) => {
   res.render('index');
 })
-
+//GET when show button clicked
 app.get('/workout', (req, res) => {
   workoutLog.find(function(err, workouts){
-    if(err) return res.status(500).send({error: 'database failure'});
+    if(err) return res.status(500).send({error: "Oops database failure!"});
     res.json(workouts);
   });
 })
-
+//POST when submit button clicked
 app.post('/workout', (req, res) => {
   let workoutData = new workoutLog();
   workoutData.name = req.body.name;
@@ -56,25 +55,24 @@ app.post('/workout', (req, res) => {
   workoutData.save(function(err){
     if(err){
       console.error(err);
-      res.json({result:0});
+      res.json({result:"Fail saving the data"});
       return;
     }
-    res.json({result:1});
+    res.json({result:"Success saving the data"});
   });
 })
-
+//DELETE when delete button clicked
 app.delete('/workout/deleteProcess/:deleteId', function(req, res){
-  workoutLog.deleteOne({ date: req.params.deleteId }, function(err, output){
-      if(err) return res.status(500).json({ error: "database failure" });
+  workoutLog.deleteOne({ date: req.params.deleteId }, function(err){
+      if(err) return res.status(500).json({ error: "Oops database failure" });
       workoutLog.find(function(err2, workouts){
-        if(err2) return res.status(500).send({error: 'database failure'});
+        if(err2) return res.status(500).send({error: "Oops database failure"});
         res.json(workouts);
       })
     })
 });
-
+//PUT when update button clicked
 app.put('/workout/updateProcess/:updateId', function(req, res){
-  console.log(req.body.date);
   workoutLog.updateOne({date:req.body.date}, {workout:req.body.workout}, function(err, output){
     if(err) res.status(500).json({ error: 'database failure' });
     if(!output.n) return res.status(404).json({ error: 'workout not found' });
